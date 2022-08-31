93e90f476a0f4d6e101073a10ac302f5187747d4
Joshua Duggan <joshua.duggan@gmail.com>
2022-08-31 19:45:05 -0400

    Refactored for Card class about to delete old...

--- src/components/Dragger.svelte
+++ src/components/Dragger.svelte
@@ -1,153 +1,243 @@
 <script>
-    import { onMount } from 'svelte';
-    import { tweened } from 'svelte/motion';
-    
-    let moving = false;
-    let touchLastX;
-    const minSkipSpeed = 0.5;
-    let svgWidth = 300;
-    let date = new Date();
-    let card = {
-        width: 88,
-        height: 196,
-        stroke: 2,
-        topMarginEm: 0.5,
-        sysDateStr: date.toLocaleDateString(undefined, {month: '2-digit', day: '2-digit', year: '2-digit'}),
-        sysTimeStr: date.toLocaleTimeString(undefined, {hour12: false}),
-        watchTimeStr: date.toLocaleTimeString(undefined, {hour12: false})
-    };
-    let sliderXInit;
-    const twn = tweened(0, {duration:0});
-    let moveLog = [];
-    let moveSpeed;
-    let divSvgWidth; // required to get around a bug(ish) svelte issue with svg width binding
-    let mouseButtDown = false;
+import { onMount } from 'svelte';
+import { tweened } from 'svelte/motion';
+import { watches, tage } from '../tracmanager.js';
 
-    onMount(() => {
-        svgWidth = divSvgWidth;
-        sliderXInit = (svgWidth - card.width) / 2;
-        twn.set(sliderXInit);
-    });
-    
-    function start(e) {
-        if (e instanceof MouseEvent && e.buttons > 1) return; // bad way to ignore mouse other than left click
-        mouseButtDown = true;
-        moving = true;
-        touchLastX = (e.targetTouches) ? e.targetTouches[0].pageX : undefined;
+let moving = false;
+let touchLastX;
+const minSkipSpeed = 0.5;
+let svgWidth = 300;
+let sliderRestX;
+const twn = tweened(0, {duration:0});
+let moveLog = [];
+let moveSpeed;
+let mouseButtDown = false;
+let date = new Date();
+
+class Card {
+    constructor(title, trac) {
+        this.width = 88;
+        this.height = 196;
+        this.stroke = 2;
+        this.topMarginEm = 0.5;
+        this.x = undefined;
+        this.title = title;
+        this.setTrac(trac);
     }
+    setTrac(trac) {
+        this.trac = trac;
+    }
+    getSysDate() { return this.trac ? this.trac.sysDate.toLocaleDateString(undefined, {month: '2-digit', day: '2-digit', year: '2-digit'}) : '' }
+    getSysTime() { return this.trac ? this.trac.sysDate.toLocaleTimeString(undefined, {hour12: false}) : '' }
+    getWatchTime() { return this.trac ? this.trac.sysDate.toLocaleTimeString(undefined, {hour12: false}) : '' }
+    getDiff() { return this.trac ? this.trac.secondsOff : ''; }
+}
+const C = new Card(); // a card to allow access to default values
+$: { // activates whenever svgWidth changes (which is bound to the svg enclosing div)
+    sliderRestX = (svgWidth - C.width) / 2;
+    twn.set(sliderRestX);
+}
+let cards = [new Card('Prev'), new Card('Oldest'), new Card('Current')]
+$: cards[0].x = $twn;
+cards[1].x = 0;
+$: cards[2].x = svgWidth - C.width;
 
-    function stop() { // called at the end of a finger/mouse drag
-        mouseButtDown = false;
-        console.log("stopping");
-        if (!moving) return;
-    
-        if (moveLog) {
-            moveSpeed = 0;
-            let cnt = 0;
-            moveLog.forEach((ml) => {
-                moveSpeed += ml.speed;
-                cnt++;
+/*// ideally this would be a class but as of now Svelte's compiler does not understand class fields
+let card = {
+    width: 88,
+    height: 196,
+    stroke: 2,
+    topMarginEm: 0.5,
+    x,
+    title,
+    trac
+};
+let cards = [{}, {}, {}];
+cards.forEach(c => Object.assign(c, card));
+$: cards[0].x = $twn;
+cards[0].name = 'Prev';
+cards[1].x = 0;
+cards[1].name = 'Oldest';
+$: cards[2].x = svgWidth - card.width;
+cards[2].name = 'Current';
+function cardInit(i, trac, name) {
+    if (!trac) return;
+    let c = cards[i];
+    c.name = name;
+    c.sysDateStr = trac.sysDate.toLocaleDateString(undefined, {month: '2-digit', day: '2-digit', year: '2-digit'});
+    c.sysTimeStr = trac.sysDate.toLocaleTimeString(undefined, {hour12: false});
+    c.watchTimeStr = trac.watchDate.toLocaleTimeString(undefined, {hour12: false});
+    c.diff = trac.secondsOff;
+}*/
+
+onMount(() => {
+    watches.subscribe(() => { retrievePrevAdjNow(); });
+    tage.subscribe((v) => { if (v == 'SYNC') retrievePrevAdjNow(); });
+});
+
+/**
+ * finds and assigns the correct tracs to the tracs array. What this generates
+ * is used by buildTableInfos to fill the infos array (table).
+ */
+ function retrievePrevAdjNow() {
+    cards.forEach(c => c.setTrac(undefined));
+    let ts = $watches[0].tracs;
+    let numSinceAdj = 0;
+    for (let i = ts.length - 1; i >= 0; i--) {
+        numSinceAdj++;
+        if (ts[i].wasWatchAdj) break;
+    }
+    if ($tage == 'RESULTS') {
+        if (numSinceAdj == 1) {
+            //tracs[2] = ts[ts.length - numSinceAdj];
+            //cardInit(2, ts[ts.length - numSinceAdj], 'Now');
+            cards[2].setTrac(ts[ts.length - numSinceAdj]); // Now
+        } else if (numSinceAdj == 2) {
+            //tracs[1] = ts[ts.length - 2];
+            //tracs[2] = ts[ts.length - 1];
+            //cardInit(1, ts[ts.length - 2], 'Prev');
+            //cardInit(2, ts[ts.length - 1], 'Now');
+            cards[1].setTrac(ts[ts.length - 2]); // Prev
+            cards[2].setTrac(ts[ts.length - 1]); // Now
+        } else {
+            //tracs[0] = ts[ts.length - numSinceAdj];
+            //tracs[1] = ts[ts.length - 2];
+            //tracs[2] = ts[ts.length - 1];
+            //cardInit(0, ts[ts.length - numSinceAdj], 'Old');
+            //cardInit(1, ts[ts.length - 2], 'Prev');
+            //cardInit(2, ts[ts.length - 1], 'Now');
+            cards[0].setTrac(ts[ts.length - 2]); // Prev
+            cards[1].setTrac(ts[ts.length - numSinceAdj]); // Old
+            cards[2].setTrac(ts[ts.length - 1]); // Now
+
+        }
+    } else {
+        if (numSinceAdj == 1) {
+            //tracs[1] = ts[ts.length - 1];
+            //cardInit(1, ts[ts.length - 1], 'Prev');
+            cards[2].setTrac(ts[ts.length - 1]); // Now
+        } else {
+            //tracs[0] = ts[ts.length - numSinceAdj];
+            //tracs[1] = ts[ts.length - 1];
+            //cardInit(0, ts[ts.length - numSinceAdj], 'Old');
+            //cardInit(1, ts[ts.length - 1], 'Prev');
+            cards[0].setTrac(ts[ts.length - 1]); // Prev
+            cards[1].setTrac(ts[ts.length - numSinceAdj]); // Old
+        }
+    }
+    svgWidth--;
+    svgWidth++;
+}
+
+function startDrag(e) {
+    if (e instanceof MouseEvent && e.buttons > 1) return; // bad way to ignore mouse other than left click
+    mouseButtDown = true;
+    moving = true;
+    touchLastX = (e.targetTouches) ? e.targetTouches[0].pageX : undefined;
+}
+
+function stopDrag() { // called at the end of a finger/mouse drag
+    mouseButtDown = false;
+    if (!moving) return;
+
+    if (moveLog) {
+        moveSpeed = 0;
+        let cnt = 0;
+        moveLog.forEach((ml) => {
+            moveSpeed += ml.speed;
+            cnt++;
+        });
+        moveSpeed /= cnt;
+        moveLog = undefined;
+    }
+    if (moveSpeed) {
+        let toLoc, respawnLoc;
+        if (moveSpeed > 0) {
+            toLoc = svgWidth - C.width;
+            respawnLoc = 0;
+        } else {
+            toLoc = 0;
+            respawnLoc = svgWidth - C.width;
+        }
+        moveSpeed -= moveSpeed * 0.35;
+        if (Math.abs(moveSpeed) > minSkipSpeed) {
+            twn.set(toLoc, spdToDur($twn, toLoc, Math.abs(moveSpeed))).then(() => {
+                twn.set(respawnLoc, {duration:0}).then(() => {stopDrag();});
             });
-            moveSpeed /= cnt;
-            moveLog = undefined;
-        }
-        if (moveSpeed) {
-            let toLoc, respawnLoc;
-            if (moveSpeed > 0) {
-                toLoc = svgWidth - card.width;
-                respawnLoc = 0;
-            } else {
-                toLoc = 0;
-                respawnLoc = svgWidth - card.width;
-            }
-            moveSpeed -= moveSpeed * 0.35;
-            if (Math.abs(moveSpeed) > minSkipSpeed) {
-                twn.set(toLoc, spdToDur($twn, toLoc, Math.abs(moveSpeed))).then(() => {
-                    twn.set(respawnLoc, {duration:0}).then(() => {stop();});
-                });
-                console.log(moveSpeed);
-               // moving = false;
-                return;
-            } else moveSpeed = undefined;
-        }
-    
-        moving = false;
-        touchLastX = undefined;
-    
-        twn.set(sliderXInit, spdToDur(sliderXInit, $twn, minSkipSpeed));
+            return;
+        } else moveSpeed = undefined;
     }
+
+    moving = false;
+    touchLastX = undefined;
+
+    twn.set(sliderRestX, spdToDur(sliderRestX, $twn, minSkipSpeed));
+}
+
+/**
+ * Converts desired speed of movement plus the distance to be covered (begin - end)
+ * into the number of millis duration that it will take.
+ * @param begin
+ * @param end
+ * @param speed pixels moved / millis elapsed
+ */
+function spdToDur(begin, end, speed) {
+    return {duration: Math.abs(end - begin) / speed};
+}
+
+function dragMove(e) { // called during a finger/mouse drag
+    if (!moving || !mouseButtDown) return;
+
+    let ml = {
+        secs: Date.now(),
+        move: (e.movementX != undefined) ? e.movementX : e.targetTouches[0].pageX - touchLastX
+    };
+    ml.speed = (moveLog && moveLog.length > 0) ? ml.move / (ml.secs - moveLog[moveLog.length - 1].secs) : undefined;
+
+    $twn = ml.move + $twn;
+    touchLastX = (e.targetTouches) ? e.targetTouches[0].pageX : undefined;
+    if ($twn < 0) $twn = 0;
+    else if ($twn >= svgWidth - C.width) $twn = svgWidth - C.width;
+
+    if (!moveLog) moveLog = [];
+    moveLog.push(ml);
+    if (moveLog.length > 5) moveLog = moveLog.splice(moveLog.length - 5);
+}
+</script>
     
-    /**
-     * 
-     * @param begin
-     * @param end
-     * @param speed pixels moved / millis elapsed
-     */
-    function spdToDur(begin, end, speed) {
-        return {duration: Math.abs(end - begin) / speed};
-    }
-    
-    function move(e) { // called during a finger/mouse drag
-        if (!moving || !mouseButtDown) return;
-    
-        let ml = {
-            secs: Date.now(),
-            move: (e.movementX != undefined) ? e.movementX : e.targetTouches[0].pageX - touchLastX
-        };
-        ml.speed = (moveLog && moveLog.length > 0) ? ml.move / (ml.secs - moveLog[moveLog.length - 1].secs) : undefined;
-    
-        $twn = ml.move + $twn;
-        touchLastX = (e.targetTouches) ? e.targetTouches[0].pageX : undefined;
-        if ($twn < 0) $twn = 0;
-        else if ($twn >= svgWidth - card.width) $twn = svgWidth - card.width;
-    
-        if (!moveLog) moveLog = [];
-        moveLog.push(ml);
-        if (moveLog.length > 5) moveLog = moveLog.splice(moveLog.length - 5);
-    }
-    </script>
-    
-    <svelte:window on:mouseup={stop} on:touchend={stop} on:touchcancel={stop} on:mousemove={move} on:touchmove={move}/>
-    <div bind:clientWidth={divSvgWidth}>
-    <svg on:mousedown={start} on:touchstart={start} width=100% height="230">
-        <defs>
-            <svg id='card' width={card.width} height={card.height}>
-                <rect x={card.stroke / 2} y={card.stroke / 2} rx="25" ry="25" width={card.width - card.stroke}
-                    height={card.height - card.stroke} stroke="black" fill="lightblue" stroke-width={card.stroke}/>
-                    <text class="ch" x="50%" y="{1 + card.topMarginEm}em">Current</text>
-                    <line x1="0" y1="2.2em" x2={card.width} y2="2.2em" stroke="black" stroke-width={card.stroke}/>
-                    <text class="ch" x="50%" y="{3 + card.topMarginEm}em">System</text>
-                    <text class="cd" x="50%" y="{4 + card.topMarginEm}em">{card.sysDateStr}</text>
-                    <text class="cd" x="50%" y="{5 + card.topMarginEm}em">{card.sysTimeStr}</text>
-                    <line x1="0" y1="6.2em" x2={card.width} y2="6.2em" stroke="black" stroke-width={card.stroke}/>
-                    <text class="ch" x="50%" y="{7 + card.topMarginEm}em">Watch</text>
-                    <text class="cd" x="50%" y="{8 + card.topMarginEm}em">{card.watchTimeStr}</text>
-                    <line x1="0" y1="9.2em" x2={card.width} y2="9.2em" stroke="black" stroke-width={card.stroke}/>
-                    <text class="ch" x="50%" y="{10 + card.topMarginEm}em">Diff</text>
-                    <text class="cd" x="50%" y="{11 + card.topMarginEm}em">-3.4s</text>
-                </svg>
-        </defs>
-    
-        <rect width="100%" height="100%" fill='lightgray'/>
-    
-        <use xlink:href='#card' x={$twn} y='10'/>
-        <use xlink:href='#card' x="0" y="10"/>
-        <use xlink:href='#card' x={svgWidth - card.width} y="10"/>
-    
-    </svg>
-    </div>
-    
-    <style>
-    svg {
-        user-select: none;
-    }
-    svg text {
-        text-anchor: middle;
-    }
-    .ch {
-        font-weight: bold;
-    }
-    .cd {
-        font-family: 'Courier New', Courier, monospace;
-    }
-    </style>
+<svelte:window on:mouseup={stopDrag} on:touchend={stopDrag} on:touchcancel={stopDrag} on:mousemove={dragMove} on:touchmove={dragMove}/>
+<div bind:clientWidth={svgWidth}><svg on:mousedown={startDrag} on:touchstart={startDrag} width=100% height="230">
+    <rect width="100%" height="100%" fill='lightgray'/>
+    {#each cards as card}
+        <svg width={card.width} height={card.height} x={card.x} y='0'>
+            <rect x={card.stroke / 2} y={card.stroke / 2} rx="25" ry="25" width={card.width - card.stroke}
+                height={card.height - card.stroke} stroke="black" fill="lightblue" stroke-width={card.stroke}/>
+            <text class="ch" x="50%" y="{1 + card.topMarginEm}em">{card.title}</text>
+            <line x1="0" y1="2.2em" x2={card.width} y2="2.2em" stroke="black" stroke-width={card.stroke}/>
+            <text class="ch" x="50%" y="{3 + card.topMarginEm}em">System</text>
+            <text class="cd" x="50%" y="{4 + card.topMarginEm}em">{card.getSysDate()}</text>
+            <text class="cd" x="50%" y="{5 + card.topMarginEm}em">{card.getSysTime()}</text>
+            <line x1="0" y1="6.2em" x2={card.width} y2="6.2em" stroke="black" stroke-width={card.stroke}/>
+            <text class="ch" x="50%" y="{7 + card.topMarginEm}em">Watch</text>
+            <text class="cd" x="50%" y="{8 + card.topMarginEm}em">{card.getWatchTime()}</text>
+            <line x1="0" y1="9.2em" x2={card.width} y2="9.2em" stroke="black" stroke-width={card.stroke}/>
+            <text class="ch" x="50%" y="{10 + card.topMarginEm}em">Diff</text>
+            <text class="cd" x="50%" y="{11 + card.topMarginEm}em">{card.getDiff()}</text>
+        </svg>
+    {/each}
+</svg></div>
+
+<style>
+svg {
+    user-select: none;
+}
+svg text {
+    text-anchor: middle;
+}
+.ch {
+    font-weight: bold;
+}
+.cd {
+    font-family: 'Courier New', Courier, monospace;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import App from './App.svelte';
 
+class TestClass {
+	fieldOne = 42;
+	fieldTwo;
+	constructor(fieldTwo) {
+		fieldTwo = fieldTwo;
+	}
+}
+
 const app = new App({
 	target: document.body,
 	props: {
